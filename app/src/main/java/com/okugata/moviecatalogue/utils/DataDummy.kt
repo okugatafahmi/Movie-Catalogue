0ba4a9f5ae752c2bd1d338358963f3734b601b60
okugatafahmi <otasrit@gmail.com>
2021-04-26 10:34:08 +0700

    chore: add tv show dummy

--- app/src/main/java/com/okugata/moviecatalogue/utils/DataDummy.kt
+++ app/src/main/java/com/okugata/moviecatalogue/utils/DataDummy.kt
@@ -121,7 +121,106 @@ object DataDummy {
                 "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
                 "Crime, Drama, Mystery, Action & Adventure",
                 "42m",
-                6
+                10
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Doom Patrol",
+                "February 15, 2019",
+                R.drawable.poster_doom_patrol,
+                "The Doom Patrol’s members each suffered horrible accidents that gave them superhuman abilities — but also left them scarred and disfigured. Traumatized and downtrodden, the team found purpose through The Chief, who brought them together to investigate the weirdest phenomena in existence — and to protect Earth from what they find.",
+                "Sci-Fi & Fantasy, Comedy, Drama",
+                "49m",
+                9
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Dragon Ball",
+                "February 26, 1986",
+                R.drawable.poster_dragon_ball,
+                "Long ago in the mountains, a fighting master known as Gohan discovered a strange boy whom he named Goku. Gohan raised him and trained Goku in martial arts until he died. The young and very strong boy was on his own, but easily managed. Then one day, Goku met a teenage girl named Bulma, whose search for the mystical Dragon Balls brought her to Goku's home. Together, they set off to find all seven and to grant her wish.",
+                "Animation, Action & Adventure, Sci-Fi & Fantasy",
+                "25m",
+                153
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Fairy Tail",
+                "October 12, 2009",
+                R.drawable.poster_fairytail,
+                "Lucy is a 17-year-old girl, who wants to be a full-fledged mage. One day when visiting Harujion Town, she meets Natsu, a young man who gets sick easily by any type of transportation. But Natsu isn't just any ordinary kid, he's a member of one of the world's most infamous mage guilds: Fairy Tail.",
+                "Action & Adventure, Animation, Comedy, Sci-Fi & Fantasy, Mystery",
+                "25m",
+                51
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Family Guy",
+                "January 31, 1999",
+                R.drawable.poster_family_guy,
+                "Sick, twisted, politically incorrect and Freakin' Sweet animated series featuring the adventures of the dysfunctional Griffin family. Bumbling Peter and long-suffering Lois have three kids. Stewie (a brilliant but sadistic baby bent on killing his mother and taking over the world), Meg (the oldest, and is the most unpopular girl in town) and Chris (the middle kid, he's not very bright but has a passion for movies). The final member of the family is Brian - a talking dog and much more than a pet, he keeps Stewie in check whilst sipping Martinis and sorting through his own life issues.",
+                "Animation, Comedy",
+                "22m",
+                20
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "The Flash",
+                "October 7, 2014",
+                R.drawable.poster_flash,
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
+                "Drama, Sci-Fi & Fantasy",
+                "44m",
+                18
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Gotham",
+                "September 22, 2014",
+                R.drawable.poster_gotham,
+                "Everyone knows the name Commissioner Gordon. He is one of the crime world's greatest foes, a man whose reputation is synonymous with law and order. But what is known of Gordon's story and his rise from rookie detective to Police Commissioner? What did it take to navigate the multiple layers of corruption that secretly ruled Gotham City, the spawning ground of the world's most iconic villains? And what circumstances created them – the larger-than-life personas who would become Catwoman, The Penguin, The Riddler, Two-Face and The Joker?",
+                "Drama, Crime, Sci-Fi & Fantasy",
+                "43m",
+                12
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Grey's Anatomy",
+                "March 27, 2005",
+                R.drawable.poster_grey_anatomy,
+                "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+                "Drama",
+                "43m",
+                16
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Hanna",
+                "March 28, 2019",
+                R.drawable.poster_hanna,
+                "This thriller and coming-of-age drama follows the journey of an extraordinary young girl as she evades the relentless pursuit of an off-book CIA agent and tries to unearth the truth behind who she is. Based on the 2011 Joe Wright film.",
+                "Action & Adventure, Drama",
+                "50m",
+                8
+            )
+        )
+        tvShows.add(
+            TvShow(
+                "Marvel's Iron Fist",
+                "March 17, 2017",
+                R.drawable.poster_iron_fist,
+                "Danny Rand resurfaces 15 years after being presumed dead. Now, with the power of the Iron Fist, he seeks to reclaim his past and fulfill his destiny.",
+                "Action & Adventure, Drama, Sci-Fi & Fantasy",
+                "55m",
+                10
             )
         )
 
