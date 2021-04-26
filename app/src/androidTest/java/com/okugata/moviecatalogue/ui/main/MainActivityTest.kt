96436ef4c07f45a6d65f14b42026f7b55414ece9
okugatafahmi <otasrit@gmail.com>
2021-04-26 16:05:56 +0700

    test: add instrumental test

---
+++ app/src/androidTest/java/com/okugata/moviecatalogue/ui/main/MainActivityTest.kt
@@ -0,0 +1,61 @@
+package com.okugata.moviecatalogue.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.okugata.moviecatalogue.R
+import com.okugata.moviecatalogue.utils.DataDummy
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityTest{
+
+    private val movies = DataDummy.generateDummyMovies()
+    private val tvShows = DataDummy.generateDummyTvShows()
+
+    @Before
+    fun setup(){
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun loadMovies() {
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(movies.size))
+    }
+
+    @Test
+    fun loadDetailMovie() {
+        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(movies[0].title)))
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_date)).check(matches(withText(movies[0].releaseDate)))
+        onView(withId(R.id.text_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_overview)).check(matches(withText(movies[0].overview)))
+    }
+
+    @Test
+    fun loadTvShows() {
+        onView(withText("TV SHOWS")).perform(click())
+        onView(withId(R.id.rv_tv_shows)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_shows)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(tvShows.size))
+    }
+
+    @Test
+    fun loadDetailTvShow() {
+        onView(withText("TV SHOWS")).perform(click())
+        onView(withId(R.id.rv_tv_shows)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_shows)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(tvShows[0].title)))
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_date)).check(matches(withText(tvShows[0].releaseDate)))
+        onView(withId(R.id.text_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_overview)).check(matches(withText(tvShows[0].overview)))
+    }
+}
