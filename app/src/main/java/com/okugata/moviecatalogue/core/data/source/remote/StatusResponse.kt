318acd9132b1201b8022d17272d8e95889261273
Okugata Fahmi <otasrit@gmail.com>
2021-05-28 17:08:01 +0700

    chore: remove unused file

--- app/src/main/java/com/okugata/moviecatalogue/core/data/source/remote/StatusResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.okugata.moviecatalogue.core.data.source.remote
-
-enum class StatusResponse {
-    SUCCESS,
-    EMPTY,
-    ERROR
-}
--- app/src/main/java/com/okugata/moviecatalogue/core/vo/Status.kt
+++
@@ -1,7 +0,0 @@
-package com.okugata.moviecatalogue.core.vo
-
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
-}
