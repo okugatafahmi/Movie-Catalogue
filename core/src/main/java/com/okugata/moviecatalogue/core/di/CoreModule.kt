f7a8a058c79efbd90feb4add2e8fbb6081c66e0e
Okugata Fahmi <otasrit@gmail.com>
2021-06-08 17:27:28 +0700

    security: encrypt database

--- core/src/main/java/com/okugata/moviecatalogue/core/di/CoreModule.kt
+++ core/src/main/java/com/okugata/moviecatalogue/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.okugata.moviecatalogue.core.data.source.remote.RemoteDataSource
 import com.okugata.moviecatalogue.core.data.source.remote.network.ApiService
 import com.okugata.moviecatalogue.core.domain.repository.ICatalogueRepository
 import com.okugata.moviecatalogue.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<CatalogueDatabase>().catalogueDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("movie_catalogue".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
-            CatalogueDatabase::class.java, "catalogue_database"
-        ).fallbackToDestructiveMigration().build()
+            CatalogueDatabase::class.java, "catalogue.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
